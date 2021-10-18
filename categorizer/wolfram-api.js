113b71ae8f793e37aae32075db22daa87f51c139
Spencer Malott <spencermalott16@gmail.com>
2021-10-18 22:42:39 +0000

    improved the wolfram api categorizing logic to handle cases where there are assumption clashes, i.e. there are multiple possible groups of assumptions. We assumed that the first group is best for now

--- categorizer/wolfram-api.js
+++ categorizer/wolfram-api.js
@@ -30,7 +30,8 @@ const fetchWolframItem = function(searchItem) {
   return request(`http://api.wolframalpha.com/v2/query?input=${searchItem}&appid=${appID}`)
     .then((result) => {
         const jsonObj = parser.parse(result, options);
-        const assumptions = jsonObj.queryresult.assumptions.assumption.value;
+        //this next line of code assumes that Wolframs first group of assumptions is best if there is a clash
+        const assumptions = jsonObj.queryresult.assumptions.assumption[0].value;
         const itemDescriptions = []
         for (let i = 0; i < assumptions.length; i++) {
           itemDescriptions.push(assumptions[i].attr.desc);
@@ -42,9 +43,9 @@ const fetchWolframItem = function(searchItem) {
     })
 };
 
-// fetchWolframItem('laptop')
-//   .then((result) => {
-//     console.log(result);
-//   })
+fetchWolframItem('christmas tree')
+  .then((result) => {
+    console.log(result);
+  })
 
 module.exports = { fetchWolframItem };
