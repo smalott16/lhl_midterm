2aa481d32a446d61ec19bdb29af5206f91cb9e43
Spencer Malott <spencermalott16@gmail.com>
2021-10-18 18:48:51 +0000

    added a new file which returns a wolfram alpha search result as a promise. The function takes a search item as the input

---
+++ categorizer/wolfram-api.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const request = require('request-promise-native');
+var parser = require('fast-xml-parser');
+
+const appID = process.env.WA_API_KEY;
+
+
+const fetchWolframItem = function(searchItem) {
+
+  const options = {
+    attributeNamePrefix : "",
+    attrNodeName: "attr", //default is 'false'
+    textNodeName : "#text",
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : false,
+    parseNodeValue : true,
+    parseAttributeValue : true,
+    trimValues: true,
+    cdataTagName: "__cdata", //default is 'false'
+    cdataPositionChar: "\\c",
+    parseTrueNumberOnly: false,
+    numParseOptions:{
+      hex: true,
+      leadingZeros: true,
+    },
+  };
+
+  return request(`http://api.wolframalpha.com/v2/query?input=${searchItem}&appid=${appID}`)
+  .then((result) => {
+      const jsonObj = parser.parse(result, options);
+      const assumptions = jsonObj.queryresult.assumptions.assumption.value;
+      const itemDescriptions = []
+      for (let i = 0; i < assumptions.length; i++) {
+        itemDescriptions.push(assumptions[i].attr.desc);
+      }
+      return itemDescriptions;
+    })
+  .catch((err) =>{
+    console.log(err.message);
+  })
+};
+
+fetchWolframItem('laptop')
+  .then((result) => {
+    console.log(result);
+  })
