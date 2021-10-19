080c66fb4bfda7dbaef49107c261ff2913279dfa
Katie <katie.herda2@gmail.com>
2021-10-19 19:47:45 +0000

    added in document.ready function to target specific list-item ID so we can use that id to edit, delete, or complete a list item in our database.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,16 @@
 // Client facing scripts here
+
+
+$(document).ready(function() {
+  $(".fa-trash").one("click", function(event) {
+    //strips text from item ID (ids cannot be numbers)
+    const itemID = this.parentElement.parentElement.id.slice(6);
+
+
+
+
+    //alert('Hello!');
+    event.stopImmediatePropagation()
+
+  });
+})
