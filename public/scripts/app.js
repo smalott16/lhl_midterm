bef052d50bbca1acbdf142261d0fd14c8b05e872
Katie <katie.herda2@gmail.com>
2021-10-19 20:58:37 +0000

    commented out client side JQuery logic as we could not access database to do what we wanted to do from here.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,22 @@
 // Client facing scripts here
 
 
-$(document).ready(function() {
-  $(".fa-trash").one("click", function(event) {
-    //strips text from item ID (ids cannot be numbers)
-    const itemID = this.parentElement.parentElement.id.slice(6);
+// $(document).ready(function() {
 
+//   $(".fa-trash").one("click", function(event) {
+//     //strips text from item ID (ids cannot be numbers)
+//     const itemID = this.parentElement.parentElement.id.slice(6);
 
+//     db.query("SELECT * FROM users;")
+//       .then((result) => {
+//         console.log(result)
+//       })
+//       .catch((err) => {
+//         console.log(err.message)
+//       })
 
+//     //alert('Hello!');
+//     event.stopImmediatePropagation()
 
-    //alert('Hello!');
-    event.stopImmediatePropagation()
-
-  });
-})
+//   });
+// })
